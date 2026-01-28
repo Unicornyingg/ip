4c12f3efc06ed96ca88f6bee216bc4055d26be33
Melvin <melvintan938@gmail.com>
2026-01-28 23:49:30 +0800

    Changed Duke to John and updated skeletal code

---
+++ src/main/java/John.java
@@ -0,0 +1,10 @@
+public class John {
+    public static void main(String[] args) {
+        String line = "----------------------------------\n";
+        System.out.println(line + "Hello! I'm John\n" +
+                        "What can I do for you?\n" + line +
+                "Bye. Hope to see you again soon!\n" + line);
+    }
+}
+
+
