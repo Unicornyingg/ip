3b18d997baac09f6306159f7899046de3829ea3f
Melvin <melvintan938@gmail.com>
2026-01-29 21:07:11 +0800

    Level 1 ECHO

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
+
+
 public class John {
+
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         String line = "----------------------------------\n";
         System.out.println(line + "Hello! I'm John\n" +
-                        "What can I do for you?\n" + line +
+                        "What can I do for you?\n" + line);
+        while(true) {
+            String word = in.nextLine();
+
+            if(word.equals("bye")) {
+                break;
+            }
+
+            System.out.println(line + word + "\n" + line);
+        }
+
+        System.out.println(line +
                 "Bye. Hope to see you again soon!\n" + line);
     }
 }
