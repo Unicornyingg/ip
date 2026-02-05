06634d4c27edfb320896428e2b3c003170a6ea2c
Melvin <melvintan938@gmail.com>
2026-02-05 23:01:05 +0800

    completed level 4

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -5,13 +5,13 @@ public class John {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] t = new Task[100];
         int taskCount = 0;
         String line = "----------------------------------\n";
         System.out.println(line + "Hello! I'm John\n" +
                         "What can I do for you?\n" + line);
         while(true) {
-            String input = in.nextLine();
+            String input = in.nextLine().strip();
 
             if(input.equals("bye")) {
                 break;
@@ -19,13 +19,34 @@ public class John {
             else if(input.equals("list")) {
                 System.out.println(line);
                 for(int i = 0; i < taskCount; i++) {
-                    System.out.println((i+1) + ". " + list[i]);
+                    System.out.println((i+1) + ". " + t[i].toString());
                 }
                 System.out.println(line);
             }
-            else {
-                System.out.println(line + "added: " + input + "\n" + line);
-                list[taskCount] = input;
+            else if(input.startsWith("mark")) {
+                int item = Integer.parseInt(input.substring(5));
+                t[item-1].markAsDone();
+                System.out.println("Nice! I've marked this task as done: \n" + t[item-1].toString());
+            }
+            else if(input.startsWith("unmark")) {
+                int item = Integer.parseInt(input.substring(7));
+                t[item-1].unmarkDone();
+                System.out.println("OK, I've marked this task as not done yet: \n" + t[item-1].toString());
+            }
+            else if(input.startsWith("todo")) {
+                t[taskCount] = new Todo(input.substring(5));
+                System.out.println("Got it. I've added this task: \n" + t[taskCount].toString() + "\n" + "Now you have " + (taskCount + 1) + " tasks in the list.");
+                taskCount++;
+
+            }
+            else if( input.startsWith("deadline")){
+                t[taskCount] = new Deadline(input.substring(9, input.indexOf("/")-1), input.substring(input.indexOf("/") + 4));
+                System.out.println("Got it. I've added this task: \n" + t[taskCount].toString() + "\n" + "Now you have " + (taskCount + 1) + " tasks in the list.");
+                taskCount++;
+            }
+            else if(input.startsWith("event")){
+                t[taskCount] = new Events(input.substring(6, input.indexOf("/from")), input.substring(input.indexOf("/from") + 6, input.indexOf("/to")), input.substring(input.lastIndexOf("/to") + 4));
+                System.out.println("Got it. I've added this task: \n" + t[taskCount].toString() + "\n" + "Now you have " + (taskCount + 1) + " tasks in the list.");
                 taskCount++;
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    public void markAsDone(){
+        isDone = true;
+    }
+
+    public void unmarkDone(){
+
+        isDone = false;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    @Override
+    public String toString(){
+        return getStatusIcon() + " " + description;
+    }
 }
+
